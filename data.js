811973064d1856027584ae19c52b1400db4c3284
SNHR05 <sitinurhanifiarohma@gmail.com>
2026-09-15 16:01:31 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -1,19 +1,34 @@
 let mahasiswa = [
-    {
-        nama: "Rizki",
-        nim: "123456789",
-        jurusan: "Teknik Informatika"                               
-    },
-    {
-        nama: "Dina",
-        nim: "987654321",
-        jurusan: "Sistem Informasi" 
-    },
-    {
-        nama: "Budi",
-        nim: "456789123",
-        jurusan: "Teknik Komputer"  
-    },
-];
+		{
+			nama: "Rizki",
+			nim: "123456789",
+			jurusan: "Teknik Informatika",
+			nilai: [
+				{ mataKuliah: "Algoritma dan Pemrograman", nilai: "A" },
+				{ mataKuliah: "Basis Data", nilai: "B+" },
+				{ mataKuliah: "Jaringan Komputer", nilai: "A-" }
+			]
+		},
+		{
+			nama: "Dina",
+			nim: "987654321",
+			jurusan: "Sistem Informasi",
+			nilai: [
+				{ mataKuliah: "Analisis Sistem Informasi", nilai: "A" },
+				{ mataKuliah: "Manajemen Proyek TI", nilai: "A-" },
+				{ mataKuliah: "Basis Data", nilai: "B+" }
+			]
+		},
+		{
+			nama: "Budi",
+			nim: "456789123",
+			jurusan: "Teknik Komputer",
+			nilai: [
+				{ mataKuliah: "Sistem Digital", nilai: "B" },
+				{ mataKuliah: "Arsitektur Komputer", nilai: "B+" },
+				{ mataKuliah: "Mikroprosesor", nilai: "A-" }
+			]
+		},
+	];
 
 export default mahasiswa;
